dabacf27cc7b91dff5f4f5f5a0286f7b81974b8a
Kramushkaa <kramarev.eg17@physics.msu.ru>
2020-11-29 18:54:59 +0300

    minor updates

--- coffee-tower/js/script.js
+++ coffee-tower/js/script.js
@@ -7,10 +7,24 @@ $(function (){
     });
 });
 
+var switchMenuToActive = function (){
+    // Убираем с home button активность
+    var classes = document.querySelector("#navHomeButton").className;
+    classes=classes.replace(new RegExp("active", "g"), "");
+    document.querySelector("#navHomeButton").className = classes;
+
+    classes = document.querySelector("#navMenuButton").className;
+    if (classes.indexOf("active") == -1){
+        classes+=" active"
+        document.querySelector("#navMenuButton").className = classes;
+    }
+};
+
 (function (global){
+
 var ct = {};
 var homeHtml = "snippets/home-snippet.html"
-
+var menuUrl = "snippets/menu-snippet.html"
 var insertHtml = function (selector, html) {
     var targetElem = document.querySelector(selector);
     targetElem.innerHTML = html;
@@ -22,6 +36,7 @@ var showLoading = function (selector) {
     insertHtml(selector,html);
 }
 
+
 document.addEventListener("DOMContentLoaded", function(event) {
     showLoading("#main-content");
     $ajaxUtils.sendGetRequest(
@@ -32,5 +47,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
         false);
 });
 
-global.$ct= ct
+// Load the menu categories view
+ct.loadMenu = function () {
+    showLoading("#main-content");
+    $ajaxUtils.sendGetRequest(
+        menuUrl,
+        function (responseText){
+            document.querySelector("#main-content").innerHTML = responseText;
+        },
+    false);
+    switchMenuToActive();
+};
+
+global.$ct= ct;
 })(window);
